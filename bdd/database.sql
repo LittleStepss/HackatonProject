66592c047a8ef99a0e29d841d7d16bc070c0bdb1
TomJegou <tomjegou10@gmail.com>
2024-01-09 09:37:57 +0100

    add test data

--- bdd/database.sql
+++ bdd/database.sql
@@ -23,3 +23,15 @@ CREATE TABLE poll (
     FOREIGN KEY (fk_id_teacher) REFERENCES teacher(teacher_id)
 );
 
+-- Password is the mail address
+insert into user(mail, status, hash) values("emma.petit@ynov.com", "student", "3e0f80bd6a34b0206ffa5a66d2a5f5ffe13697783621db3d523e3597617db25e");
+insert into user(mail, status, hash) values("lucas.dupont@ynov.com", "student", "93ddae0009abc120c80d0488628077ad3bc6869ea85ddc367368c47418cef44d");
+insert into user(mail, status, hash) values("jade.garcia@ynov.com", "student", "3e271b6547b4d54cc2ab23aba9f2ff13c20f0782d0e7de68e3c5c1d456fd976f");
+insert into user(mail, status, hash) values("hugo.leroy@ynov.com", "admin", "49abfcad4bf7cbce9e3efc256b47f9eb18276bf2490678bab5a2b59fa03d4407");
+insert into user(mail, status, hash) values("enzo.sanchez@ynov.com", "admin", "b9a1b934bd99f2435b33f89149122100b3ebc4d2f86643481c7f1009d8de0ad7");
+
+INSERT INTO teacher(firstname, lastname, sector, module) 
+VALUES 
+    ('Noa', 'Martin', 'informatique', 'python'),
+    ('Pierre', 'Lefevre', 'informatique', 'php'),
+    ('Sophie', 'Dubois', 'informatique', 'c#');
