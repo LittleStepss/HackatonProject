13bdd5872f751832cb1e14af6adf4edfdbb3b81b
TomJegou <tomjegou10@gmail.com>
2024-01-09 21:30:02 +0100

    refactoring

--- api/main.go
+++ api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,15 +14,10 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("API is up ! 👍\n"))
 	})
-
-	http.HandleFunc("/api/database/getTeacher", GetTeachers)
-
 	http.HandleFunc("/teacher", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, this is a test %q")
+
 	})
 
 	fmt.Printf("api is up on address: 0.0.0.0:%d http://localhost:%d🔥\n", port, port)
 	log.Printf("http.ListenAndServe: %v", http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
-
-
