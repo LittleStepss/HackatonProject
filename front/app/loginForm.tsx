4b4c257292045400f309a565fdb90e10d2e4e8e7
TomJegou <tomjegou10@gmail.com>
2024-01-11 09:31:12 +0100

    front

--- front/app/loginForm.tsx
+++ front/app/loginForm.tsx
@@ -1,14 +1,15 @@
 export default function LoginForm() {
     return (
-        <form className="flex flex-col flex-wrap justify-center items-center gap-6">
+        <form className="flex flex-col flex-wrap justify-center items-center gap-6 border-b-2 p-12">
+            <h2 className="text-4xl absolute top-60">Bienvenue</h2>
             <div>    
                 <div className="flex flex-col flex-wrap justify-center items-start">
                     <label>Mail</label>
-                    <input type="mail" className="border-2 rounded-lg border-[#E2E1E5]" />
+                    <input type="mail" className="border-2 rounded-lg border-[#E2E1E5] p-1" />
                 </div>
                 <div className="flex flex-col flex-wrap justify-center items-start">
                     <label>Mot de passe</label>
-                    <input type="password" className="border-2 rounded-lg border-[#E2E1E5]" />
+                    <input type="password" className="border-2 rounded-lg border-[#E2E1E5] p-1" />
                 </div>
             </div>
             <input type="submit" value="connexion" className="bg-[#64BDC2] p-4 rounded-lg text-[#FFFF]" />
