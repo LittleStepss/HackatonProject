bb36f90dcfb7dbf83df30e37178da8a4f9af69b8
letienne <etienne.lemee@ynov.com>
2024-01-11 10:03:03 +0100

    added reported polls table and filled it with examples

--- bdd/database.sql
+++ bdd/database.sql
@@ -23,6 +23,18 @@ CREATE TABLE poll (
     FOREIGN KEY (fk_id_teacher) REFERENCES teacher(teacher_id)
 );
 
+CREATE TABLE reported_polls (
+    reported_poll_id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_poll_id INT NOT NULL,
+    fk_user_mail VARCHAR(100) NOT NULL,
+    fk_user_status VARCHAR(100) NOT NULL,
+    fk_poll_comment TEXT NOT NULL,
+    FOREIGN KEY (fk_poll_id) REFERENCES poll(poll_id),
+    FOREIGN KEY (fk_user_mail) REFERENCES user(mail),
+    FOREIGN KEY (fk_user_status) REFERENCES user(status),
+    FOREIGN KEY (fk_poll_comment) REFERENCES poll(comment)
+);
+
 -- Password is the mail address
 insert into user(mail, status, hash) values("emma.petit@ynov.com", "student", "3e0f80bd6a34b0206ffa5a66d2a5f5ffe13697783621db3d523e3597617db25e");
 insert into user(mail, status, hash) values("lucas.dupont@ynov.com", "student", "93ddae0009abc120c80d0488628077ad3bc6869ea85ddc367368c47418cef44d");
@@ -35,3 +47,10 @@ VALUES
     ('Noa', 'Martin', 'informatique', 'python'),
     ('Pierre', 'Lefevre', 'informatique', 'php'),
     ('Sophie', 'Dubois', 'informatique', 'c#');
+
+INSERT INTO reported_polls(fk_user_mail, fk_user_status, fk_poll_comment) 
+VALUES 
+    ('lucas.dupont@ynov.com', 'student', 'trop debile le prof'),
+    ('jade.garcia@ynov.com', 'student', 'nul éclaté'),
+    ('hugo.leroy@ynov.com', 'admin', 'Abusé trop naze '), /*admin can be reported*/
+    ('emma.petit@ynov.com', 'student', 'je me suis endormis tellement il est nul et bête');
